54937405a2dd8fe47dc8d1ad1482c18527f7cb59
wang-jie <421004034@qq.com>
2026-01-26 15:09:37 +0800

    修改下os分隔符号

--- docs/sidebar.js
+++ docs/sidebar.js
@@ -1,4 +1,4 @@
-var sidebarTxt='* [首页](/)\n';
+var sidebarTxt = '* [首页](/)\n';
 var path = require('path');
 var curPath = path.resolve('./');
 
@@ -10,39 +10,41 @@ function walkSync(currentDirPath, prefixBlank, callback) {
         var stat = fs.statSync(filePath);
         if (stat.isFile()) {
             callback(filePath, stat);
-        } else if (stat.isDirectory() && ".git"!=path.basename(filePath) && '_' != path.basename(filePath).substr(0,1)) {
-            sidebarTxt += prefixBlank +'* ' +path.basename(filePath)+'\n';
-            walkSync(filePath, prefixBlank+'  ', callback);
+        } else if (stat.isDirectory() && ".git" != path.basename(filePath) && '_' !== path.basename(filePath).slice(0, 1)) {
+            sidebarTxt += prefixBlank + '* ' + path.basename(filePath) + '\n';
+            walkSync(filePath, prefixBlank + '  ', callback);
         }
     });
 }
-walkSync(curPath,'', function(filePath, stat) {
-        if(".md" == path.extname(filePath).toLowerCase()
-            && '_' != path.basename(filePath).substr(0,1)
-            && 'README.md' != path.basename(filePath)){
-            var relativeFilePath = filePath.substr(curPath.length);
-            //console.log("file:"+ path.basename(filePath).slice(1));
-            var itemText = relativeFilePath.substr(1, relativeFilePath.length-4);
-            while(itemText.indexOf('/')>0){
-                itemText = itemText.substr(itemText.indexOf('/')+1);
-                sidebarTxt += '  ';
-            }
 
-        sidebarTxt += '- ['+itemText+']('+relativeFilePath+')\n';
-
+walkSync(curPath, '', function (filePath, stat) {
+    if (".md" == path.extname(filePath).toLowerCase()
+        && '_' != path.basename(filePath).substr(0, 1)
+        && 'README.md' != path.basename(filePath)) {
+        var relativeFilePath = filePath.substr(curPath.length);
+        //console.log("file:"+ path.basename(filePath).slice(1));
+        var itemText = relativeFilePath.substr(1, relativeFilePath.length - 4);
+        while (itemText.indexOf(path.sep) > 0) {
+            itemText = itemText.substr(itemText.indexOf(path.sep) + 1);
+            sidebarTxt += '  ';
         }
-        //console.log("file:"+ +path.extname(filePath));
+
+        sidebarTxt += '- [' + itemText + '](' + relativeFilePath.replace(/\\/g, '/') + ')\n';
+
+    }
+    //console.log("file:"+ +path.extname(filePath));
 });
 
 var path = require('path');
 var fs = require('fs');
-fs.copyFile(path.resolve('./')+"/_sidebar.md",path.resolve('./')+"/_sidebar.md",function(err){
-        if(err) throw new Error('something wrong was happended') });
+fs.copyFile(path.resolve('./') + "/_sidebar.md", path.resolve('./') + "/_sidebar.md", function (err) {
+    if (err) throw new Error('something wrong was happended')
+});
 //console.log(path.resolve('./')+"/_sidebar.md");
 
 console.log(sidebarTxt);
-fs.writeFile(path.resolve('./')+'/_sidebar.md', sidebarTxt,function(err){
-    if(err){
+fs.writeFile(path.resolve('./') + '/_sidebar.md', sidebarTxt, function (err) {
+    if (err) {
         console.error(err);
     }
 });
